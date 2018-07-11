928885c819e166e93309cce0a7f27836c3a76b0a
wangxuguang <nova_wangxuguang@126.com>
2018-07-11 15:29:36 +0800

    dart学习

---
+++ bin/main.dart
@@ -0,0 +1,11 @@
+//import 'package:DartDemoConsole/DartDemoConsole.dart' as DartDemoConsole;
+
+main(List<String> arguments) {
+  print('Hello world!');
+
+  //list
+  var list = [1, 2, 3];
+
+  list.forEach((item) => print('${list.indexOf(item)}: $item'));
+
+}
