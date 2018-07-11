64ee01b11f6d093bf7ee0dcf4755b80b20c370f5
wangxuguang <nova_wangxuguang@126.com>
2018-07-11 18:31:38 +0800

    提交dart基本语法学习

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,28 @@
 //import 'package:DartDemoConsole/DartDemoConsole.dart' as DartDemoConsole;
 
+Function makeAdder(num addBy) {
+  return (num i) => addBy + i;
+}
+
 main(List<String> arguments) {
-  print('Hello world!');
+//  print('Hello world!');
 
   //list
-  var list = [1, 2, 3];
+//  var list = [1, 2, 3];
+//
+//  list.forEach((item) => print('${list.indexOf(item)}: $item'));
 
-  list.forEach((item) => print('${list.indexOf(item)}: $item'));
+  // Create a function that adds 2.
+  var add2 = makeAdder(2);
 
+  // Create a function that adds 4.
+  var add4 = makeAdder(4);
+
+  assert(add2(3) == 5);
+  assert(add4(3) == 7);
+
+// ?? 表达式
+  String playerName(String name) => name ?? 'Guest';
+
+  print(playerName("aa"));
 }
